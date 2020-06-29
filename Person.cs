c80b6ac7b218bee224fb37557a1c808467b76d6a
Bat Betty <sprinklezap@gmail.com>
2020-06-30 01:48:07 +0400

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jey_sun
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string[] Hobby { get; set;}
+
+        public override string ToString() //вывести карточку на экран
+        {
+            string hoby = ""; //перебрать все хобби
+
+            for(int i = 0; i < Hobby.Length; i++)
+            {
+                if (i < Hobby.Length - 1) { hoby += Hobby[i] + ", "; } //если не последний
+                else { hoby += Hobby[i] + "."; }
+            }
+
+            return "\nИмя: " + Name + ", \nВозраст: " + Age + " лет, " + "\nХобби: " + hoby;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace jey_sun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pers1 = new Person() { Name = "Tom", Age = 20, Hobby = new string[1] { "Футбол" } };
+            Person pers2 = new Person() { Name = "Mike", Age = 21, Hobby = new string[2] { "Математика", "Программирование" } };
+
+            Console.WriteLine("Исходный объект");
+            Console.WriteLine(pers1);
+            Console.WriteLine();
+
+            Console.WriteLine("Исходный объект в Json");
+            string json = JsonSerializer.Serialize<Person>(pers1);
+            Console.WriteLine(json);
+
+            Console.WriteLine("Исходный объект из Json");
+            Person pers1DS = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(pers1DS);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Исходный объект2");
+            Console.WriteLine(pers2);
+            Console.WriteLine();
+
+            Console.WriteLine("Исходный объект в Json");
+            json = JsonSerializer.Serialize<Person>(pers2);
+            Console.WriteLine(json);
+
+            Console.WriteLine("Исходный объект из Json");
+            Person pers2DS = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(pers2DS);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
